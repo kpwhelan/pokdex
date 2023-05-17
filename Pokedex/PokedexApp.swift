40ac7b49d2502cffd9d2ee2230ed4d2b651ce504
kpwhelan <60331971+kpwhelan@users.noreply.github.com>
2023-05-17 12:42:32 -0400

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Kevin on 5/17/23.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
